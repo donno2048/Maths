24e2b6f765b4f81250f32ca25affcfbca41a66ed
Elisha Hollander <just4now666666@gmail.com>
2022-01-14 01:38:56 +0200

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 setup(
     name='PyMathRender',
-    version='1.0.0',
+    version='1.0.1',
     description='Pretty rendering method for mathematic functions',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
